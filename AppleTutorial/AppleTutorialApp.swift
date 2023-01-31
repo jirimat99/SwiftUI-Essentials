3d8c25329df2fb6bdb650def78e64357df3c260d
jirimatousek <jiri.matousek1999@gmail.com>
2023-01-31 10:44:07 +0100

    Initial Commit

---
+++ AppleTutorial/AppleTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleTutorialApp.swift
+//  AppleTutorial
+//
+//  Created by Jiri Matousek on 31.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct AppleTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
